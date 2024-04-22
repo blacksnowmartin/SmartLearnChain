3d945f6e38e2f240ef8f1a3a8799f40a18748d24
blacksnowmartin <martinmwendwa005@gmail.com>
2024-04-22 23:37:26 +0300

    fixed the smart contract

--- SLCToken.sol
+++ SLCToken.sol
@@ -1,456 +1,230 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.14;
 
-interface IERC20 {
+/**
+ * @title SmartChainLearn ERC20 Token Contract
+ * @dev This contract is a basic ERC20 token with some added functionality.
+ *      It is intended to be used as a template for new projects.
+ *
+ *      The contract will be used to create a new token that will be used
+ *      to incentivize people to participate in a community. The token will
+ *      be used to represent ownership of the community's digital assets.
+ *      The token is NOT meant to be a store of value.
+ *
+ *      The contract is based on the OpenZeppelin ERC20 contract and has been
+ *      modified to add functionality that is specific to the needs of the
+ *      community.
+ *
+ *      The contract has been tested on the Ethereum blockchain using the
+ *      Remix online compiler.
+ *
+ * @author blacksnowmartin
+ */
 
+
+// File: contracts/IERC20.sol
+
+/**
+ * @dev Interface of the ERC20 standard as defined in the EIP.
+ */
+interface IERC20 {
+    /**
+     * @dev Emitted when `value` tokens are moved from one account (`from`) to
+     * another (`to`).
+     *
+     * Note that `value` may be zero.
+     */
     event Transfer(address indexed from, address indexed to, uint256 value);
 
+    /**
+     * @dev Emitted when the allowance of `spender` for `owner` is set by
+     * a call to {approve}. `value` is the new allowance.
+     */
     event Approval(address indexed owner, address indexed spender, uint256 value);
 
+    /**
+     * @dev Returns the amount of tokens in existence.
+     */
     function totalSupply() external view returns (uint256);
 
+    /**
+     * @dev Returns the amount of tokens owned by `account`.
+     */
     function balanceOf(address account) external view returns (uint256);
 
-    function transfer(address to, uint256 amount) external returns (bool);
+    /**
+     * @dev Moves `amount` tokens from the caller's account to `recipient`.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transfer(address recipient, uint256 amount) external returns (bool);
 
+    /**
+     * @dev Returns the remaining number of tokens that `spender` will be
+     * allowed to spend on behalf of `owner` through {transferFrom}. This is
+     * zero by default.
+     *
+     * This value changes when {approve} or {transferFrom} are called.
+     */
     function allowance(address owner, address spender) external view returns (uint256);
 
+    /**
+     * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * IMPORTANT: Beware that changing an allowance with this method brings the risk
+     * that someone may use both the old and the new allowance by unfortunate
+     * transaction ordering. One possible solution to mitigate this race
+     * condition is to first reduce the spender's allowance to 0 and set the
+     * desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     *
+     * Emits an {Approval} event.
+     */
     function approve(address spender, uint256 amount) external returns (bool);
 
-    function transferFrom(
-
-        address from,
-
-        address to,
-
-        uint256 amount
-
-    ) external returns (bool);
-
+    /**
+     * @dev Moves `amount` tokens from `sender` to `recipient` using the
+     * allowance mechanism. `amount` is then deducted from the caller's
+     * allowance.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
 }
 
-interface IERC20Metadata is IERC20 {
+// File: contracts/IERC20Metadata.sol
 
+/**
+ * @dev Interface for the optional metadata functions from the ERC20 standard.
+ *
+ * _Available since v4.1._
+ */
+interface IERC20Metadata is IERC20 {
+    /**
+     * @dev Returns the name of the token.
+     */
     function name() external view returns (string memory);
 
+    /**
+     * @dev Returns the symbol of the token.
+     */
     function symbol() external view returns (string memory);
 
+    /**
+     * @dev Returns the decimals places of the token.
+     */
     function decimals() external view returns (uint8);
-
 }
 
+// File: contracts/Context.sol
+
+/*
+ * @dev Provides information about the current execution context, including the
+ * sender of the transaction and its data. While these are generally available
+ * via msg.sender and msg.data, they should not be accessed in such a direct
+ * manner, since when dealing with meta-transactions the account sending and
+ * paying for execution may not be the actual sender (as far as an application
+ * is concerned).
+ *
+ * This contract is only required for intermediate, library-like contracts.
+ */
 abstract contract Context {
-
     function _msgSender() internal view virtual returns (address) {
-
         return msg.sender;
-
     }
 
     function _msgData() internal view virtual returns (bytes calldata) {
-
         return msg.data;
-
     }
-
 }
 
-library SafeMath {
+// File: contracts/Ownable.sol
 
-    function add(uint256 a, uint256 b) internal pure returns (uint256) {
-
-        uint256 c = a + b;
-
-        require(c >= a, "SafeMath: addition overflow");
-
-        return c;
-
-    }
-
-    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
-
-        return sub(a, b, "SafeMath: subtraction overflow");
-
-    }
-
-    function sub(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
-
-        require(b <= a, errorMessage);
-
-        uint256 c = a - b;
-
-        return c;
-
-    }
-
-    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
-
-        if (a == 0) {
-
-        return 0;
-
-        }
-
-        uint256 c = a * b;
-
-        require(c / a == b, "SafeMath: multiplication overflow");
-
-        return c;
-
-    }
-
-    /**
-    Clarification needed over this function
-     */
-
-    function div(uint256 a, uint256 b) internal pure returns (uint256) {
-
-        return div(a, b, "SafeMath: division by zero");
-
-    }
-
-    function div(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
-
-        require(b > 0, errorMessage);
-
-        uint256 c = a / b;
-
-        return c;
-
-    }
-
-}
-
-
+/**
+ * @dev Contract module which provides a basic access control mechanism, where
+ * there is an account (an owner) that can be granted exclusive access to
+ * specific functions.
+ *
+ * By default, the owner account will be the one that deploys the contract. This
+ * can later be changed with {transferOwnership}.
+ *
+ * This module is used through inheritance. It will make available the modifier
+ * `onlyOwner`, which can be applied to your functions to restrict their use to
+ * the owner.
+ */
 abstract contract Ownable is Context {
-
     address private _owner;
 
     event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
 
+    /**
+     * @dev Initializes the contract setting the deployer as the initial owner.
+     */
     constructor() {
-
-        _transferOwnership(_msgSender());
-
+        _setOwner(_msgSender());
     }
-
-    modifier onlyOwner() {
-
-        _checkOwner();
-
-        _;
-
-    }
-
-    function owner() public view virtual returns (address) {
-
-        return _owner;
-
-    }
-
-    function _checkOwner() internal view virtual {
-
-        require(owner() == _msgSender(), "Ownable: caller is not the owner");
-
-    }
-
-    function renounceOwnership() public virtual onlyOwner {
-
-        _transferOwnership(address(0));
-
-    }
-
-    function transferOwnership(address newOwner) public virtual onlyOwner {
-
-        require(newOwner != address(0), "Ownable: new owner is the zero address");
-
-        _transferOwnership(newOwner);
-
-    }
-
-    function _transferOwnership(address newOwner) internal virtual {
-
-        address oldOwner = _owner;
-
-        _owner = newOwner;
-
-        emit OwnershipTransferred(oldOwner, newOwner);
-
-    }
-
-}
-
-contract SmartChainLearn is IERC20, Ownable {
-
-    using SafeMath for uint256;
-
-    string private constant _name = "SmartChainLearn";
-
-    string private constant _symbol = "SCL";
-
-    uint8 private constant _decimals = 18;
-
-    mapping(address => uint256) private _tOwned;
-    
-    address payable private _SmartChainLearn;
-
-    mapping(address => mapping(address => uint256)) private _allowances;
-
-    uint256 private constant _tTotal = 21000000 * 10**18;
-
-    address public uniswapV2Pair;
-
-    bool private inSwap = false;
-
-    bool private swapEnabled = true;
-
-    modifier lockTheSwap {
-
-        inSwap = true;
-
-        _;
-
-        inSwap = false;
-
-    }
-
-    modifier onlySmartChainLearn{
-
-        require(_msgSender() == _SmartChainLearn, "Only SmartChainLearn ");
-
-        _;
-
-    }
-
-    constructor(address _SmartChainLearnAddress) {
-
-        _SmartChainLearn = payable(_SmartChainLearnAddress);
-
-        _mint(_SmartChainLearn, _tTotal);
-
-    }
-
-    function name()  public pure returns (string memory) {
-
-        return _name;
-
-    }
-
-    function symbol()  public pure returns (string memory) {
-
-        return _symbol;
-
-    }
-
-    function decimals()  public pure returns (uint8) {
-
-        return _decimals;
-
-    }
-
-    function totalSupply() public pure returns (uint256) {
-
-        return _tTotal;
-
-    }
-
-    function balanceOf(address account) public view returns (uint256){
-
-        return _tOwned[account];
-
-    }
-
-    function transfer(address recipient, uint256 amount) public  override returns (bool) {
-
-        _transfer(_msgSender(), recipient, amount);
-
-        return true;
-
-    }
-
-    function allowance(address owner, address spender) public view override returns (uint256) {
-
-        return _allowances[owner][spender];
-
-    }
-
-    function approve(address spender, uint256 amount) public override returns (bool)
-
-    {
-
-        _approve(_msgSender(), spender, amount);
-
-        return true;
-
-    }
-
-    function transferFrom( address sender, address recipient, uint256 amount ) public override returns (bool) {
-
-        _transfer(sender, recipient, amount);
-
-        _approve(sender, _msgSender(), _allowances[sender][_msgSender()].sub(amount, "ERC20: transfer amount exceeds allowance"));
-
-        return true;
-
-    }
-
-    function withdrawStuckETH() public {
-
-        bool success;
-
-        (success,) = address(_SmartChainLearn).call{value: address(this).balance}("");
-
-    }
-
-    function _approve( address owner, address spender, uint256 amount ) internal {
-
-        require(owner != address(0), "ERC20: approve from the zero address");
-
-        require(spender != address(0), "ERC20: approve to the zero address");
-
-        _allowances[owner][spender] = amount;
-
-        emit Approval(owner, spender, amount);
-
-    }
-
-    function _transfer(address from, address to, uint256 amount ) internal {
-
-        require(from != address(0), "ERC20: transfer from the zero address");
-
-        require(to != address(0), "ERC20: transfer to the zero address");
-
-        require(amount > 0, "Transfer amount must be greater than zero");
-
-        _tokenTransfer(from, to, amount);
-
-    }
-
-
-    function sendETHToFee(uint256 amount) private {
-
-        _SmartChainLearn.transfer(amount);
-
-    }
-
-    function manualsend() external {
-
-        require(_msgSender() == _SmartChainLearn);
-
-        uint256 contractETHBalance = address(this).balance;
-
-        sendETHToFee(contractETHBalance);
-
-    }
-
-    function _tokenTransfer(address sender, address recipient, uint256 amount) private {
-
-        _transferStandard(sender, recipient, amount);
-
-    }
-
-    function _transferStandard(address sender, address recipient, uint256 tAmount) private {
-
-        (uint256 tTransferAmount, uint256 tTeam) = _getValues(tAmount);
-
-        _tOwned[sender] = _tOwned[sender].sub(tAmount);
-
-        _tOwned[recipient] = _tOwned[recipient].add(tTransferAmount);
-
-        _takeTeam(sender, tTeam);
-
-        emit Transfer(sender, recipient, tTransferAmount);
-
-    }
-
-    function _takeTeam(address sender, uint256 tTeam) private {
-
-        if(tTeam > 0){
-
-            _tOwned[_SmartChainLearn] = _tOwned[_SmartChainLearn].add(tTeam);
-
-            emit Transfer(sender, _SmartChainLearn, tTeam);
-
-        }
-
-    }
-
-    receive() external payable {}
-
-    function _getValues( uint256 tAmount) private pure returns ( uint256, uint256 ) {
-
-        uint256 tTeam = tAmount.div(100);
-
-        uint256 tTransferAmount = tAmount.sub(tTeam);
-
-        return (tTransferAmount, tTeam);
-
-    }
-
-
-    function toggleSwap(bool _swapEnabled) public onlyOwner {
-
-        swapEnabled = _swapEnabled;
-
-    }
-
-    function _mint(address account, uint256 amount) internal virtual {
-
-        require(account != address(0), "ERC20: mint to the zero address");
-
-        unchecked {
-
-            _tOwned[account] += amount;
-
-        }
-
-        emit Transfer(address(0), account, amount);
-
-    }
-
-}
-
 
     /**
-     * @dev Creates `amount` tokens and assigns them to `account`, increasing
-     * the total supply.
-     *
-     * Emits a {Transfer} event with `from` set to the zero address.
-     *
-     * Requirements:
-     *
-     * - `account` cannot be the zero address.
+     * @dev Returns the address of the current owner.
      */
+    function owner() public view virtual returns (address) {
+        return _owner;
+    }
 
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        require(owner() == _msgSender(), "Ownable: caller is not the owner");
+        _;
+    }
 
+    /**
+     * @dev Leaves the contract without owner. It will not be possible to call
+     * `onlyOwner` functions anymore. Can only be called by the current owner.
+     *
+     * NOTE: Renouncing ownership will leave the contract without an owner,
+     * thereby removing any functionality that is only available to the owner.
+     */
+    function renounceOwnership() public virtual onlyOwner {
+        _setOwner(address(0));
+    }
 
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     */
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        _setOwner(newOwner);
+    }
 
+    function _setOwner(address newOwner) private {
+        address oldOwner = _owner;
+        _owner = newOwner;
+        emit OwnershipTransferred(oldOwner, newOwner);
+    }
+}
 
+// File: contracts/SafeMath.sol
 
+// CAUTION
+// This version of SafeMath should only be used with Solidity 0.8 or later,
+// because it relies on the compiler's built in overflow checks.
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/**
+ * @dev Wrappers over Solidity's arithmetic operations.
+ *
+ * NOTE: `SafeMath` is generally not needed starting with Solidity 0.8, since the compiler
+ * now has built in overflow checking.
+ */
+library SafeMath {
+    /**
+     * @dev Returns the addition of two unsigned integers, with an overflow flag.
 
