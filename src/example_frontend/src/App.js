02d60db9b98b4e84306f27816a796173b670d279
blacksnowmartin <martinmwendwa005@gmail.com>
2024-10-05 16:14:37 +0300

    Saved

--- src/example_frontend/src/App.js
+++ src/example_frontend/src/App.js
@@ -1,5 +1,5 @@
 import { html, render } from 'lit-html';
-import { example_backend } from 'declarations/example_backend';
+import { example_backend } from '../../declarations/example_backend';
 import logo from './logo2.svg';
 
 class App {
